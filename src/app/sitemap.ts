5798fd22805cae9c1084c1d5a6860cc0d79d21eb
Bernard Ibrovic <bernardibrovic@Bernards-MBP.home>
2025-09-05 17:51:26 +0200

    Add PDV calculator to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -78,6 +78,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       changeFrequency: "monthly",
       priority: 0.6,
     },
+    {
+      url: `${baseUrl}/alati/pdv-kalkulator`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.6,
+    },
 
   ];
 
